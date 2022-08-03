9f31b299bba99a7b74af47174425a4fd4f2e2dfa
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-08-03 16:03:01 +0300

    fix

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,12 @@
+
+import PropTypes from 'prop-types';
+import {useStyles}from './styles'
+
+function Container({ children }) {
+  const styles = useStyles()
+  return <div className={styles.container}>{children}</div>;
+}
+Container.propTypes = {
+  children: PropTypes.array.isRequired,
+};
+export default Container;
